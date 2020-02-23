bc01f719d039d447dcfe6b35968c9a95ff85a0ec
lfn3 <liam@lfn3.net>
2020-02-23 15:45:30 +0000

    Tests.
    Abandoned the decorator idea,
    the whole global state thing was too
    squirrely for my liking

--- kubephemeral/main.py
+++ kubephemeral/main.py
@@ -16,31 +16,32 @@ def apply_manifests(namespace: str, manifests: List[Path]):
 
 
 class EphermeralNameSpace:
-    def __init__(self, api_client: client.CoreV1Api, name: str):
+    def __init__(self, api_client: client.ApiClient, name: str):
         self.name = name
         self.api_client = api_client
+        self.core_v1_api = client.CoreV1Api(api_client)
 
     def __enter__(self):
         return self
 
     def __exit__(self, *args, **kwargs):
-        self.api_client.delete_namespace(self.name)
+        self.core_v1_api.delete_namespace(self.name)
         logging.info(f"Deleted {self}")
 
     def __str__(self):
-        return f"EphermeralNameSpace[{self.name}] on [{self.api_client.api_client.configuration.host}]"
+        return f"EphermeralNameSpace[{self.name}] on [{self.api_client.configuration.host}]"
 
     def apply_manifests(self, manifests: List[Path]):
         apply_manifests(self.name, manifests)
 
 
-def create_named_ns(name: str, api: client.CoreV1Api):
+def create_named_ns(name: str, api: client.ApiClient):
     meta = client.V1ObjectMeta(name=name)
     ns = client.V1Namespace(metadata=meta)
-    api.create_namespace(ns)
+    client.CoreV1Api(api).create_namespace(ns)
 
 
-def create_ns(api: client.CoreV1Api) -> EphermeralNameSpace:
+def create_ns(api: client.ApiClient) -> EphermeralNameSpace:
     """Returns the (randomly generated) name of the created namespace"""
     name = f'{random.getrandbits(64)}-{int(time.time())}'
     create_named_ns(name, api)
@@ -49,23 +50,6 @@ def create_ns(api: client.CoreV1Api) -> EphermeralNameSpace:
     return ns
 
 
-def with_manifests(manifests):
-    def inner(fn):
-        fn()
-
-    return inner
-
-
-def api_client_from_config() -> client.CoreV1Api:
+def api_client_from_config() -> client.ApiClient:
     configuration = config.load_kube_config()
-    return client.CoreV1Api(client.ApiClient(configuration))
-
-
-def main():
-    api = api_client_from_config()
-    with create_ns(api) as ns:
-        ns.apply_manifests([Path('job.yaml')])
-
-
-if __name__ == '__main__':
-    main()
+    return client.ApiClient(configuration)
--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,37 @@
 from pathlib import Path
 
-from main import with_manifests, api_client_from_config, EphermeralNameSpace
+from kubephemeral.main import api_client_from_config, create_ns
+from kubernetes import client
 
 
-@with_manifests([Path('..') / 'job.yaml'])
-def test_fixture(kubephemeral: EphermeralNameSpace):
-    # TODO: get job
-    pass
+def test_creates_namespace():
+    api = api_client_from_config()
+    with create_ns(api) as ns:
+        ns_names = [ns.metadata.name for ns in client.CoreV1Api(api).list_namespace().items]
+        assert ns.name in ns_names
+
+
+def test_cleans_up_namespace():
+    api = api_client_from_config()
+    with create_ns(api) as eph_ns:
+        ns_names = [ns.metadata.name for ns in client.CoreV1Api(api).list_namespace().items]
+        assert eph_ns.name in ns_names
+
+    matching_nses = [ns for ns in client.CoreV1Api(api).list_namespace().items if ns.metadata.name == eph_ns.name]
+    if matching_nses == 0:
+        return  # Already terminated, success!
+
+    # Or, there should only be one...
+    assert len(matching_nses) == 1
+    k8s_ns = matching_nses[0]
+    # ... that is being cleaned up.
+    assert k8s_ns.status.phase == 'Terminating'
+
+
+def test_with_manifests():
+    api = api_client_from_config()
+    job_api = client.BatchV1Api(api)
+    with create_ns(api) as eph_ns:
+        eph_ns.apply_manifests([Path("..") / "job.yaml"])
+        jobs = job_api.list_namespaced_job(eph_ns.name).items
+        assert len(jobs) == 1
